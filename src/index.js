c4675c2d7124240e20402dff395a30ebe60cbd03
chi8708 <983143462@qq.com>
2018-03-31 00:58:32 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var str=require('./str.js');
+var css=require('./index.css');
+//document.write(str.hello);
+document.write(str('<div>你好呀</div>'));
---
+++ src/str.js
@@ -0,0 +1,8 @@
+var data={
+    hello:'今天天气很好'
+}
+//module.exports=data;
+function hello(str){
+  return str;
+}
+module.exports=hello;
